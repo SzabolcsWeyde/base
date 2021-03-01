4345cf700ece9fbcf0ade00eb3c7a6542ea8543d
Szabi <Szabi@192.168.0.17>
2021-03-01 02:21:12 +0100

    l

---
+++ train-controller/src/test/java/hu/bme/mit/train/controller/TrainControllerTest.java
@@ -0,0 +1,22 @@
+package hu.bme.mit.train.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class TrainControllerTest {
+
+	TrainControllerImpl controller
+	
+	
+    @Before
+    public void before() {
+        controller= new TrainControllerImpl();
+    }
+
+    @Test
+    public void ThisIsAnExampleTestStub() {
+    	Assert.assertEquals(10, controller.getReferenceSpeed());
+    }
+}
